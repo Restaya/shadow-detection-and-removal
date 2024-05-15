c33dfdf1020b174b90da4d041b7d2336b18063d0
Fehér Gábor <h982734@stud.u-szeged.hu>
2024-05-15 14:31:21 +0200

    Implemented metric calculation

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import cv2
+import os
+
 from shadow_detection import *
 from shadow_removal import *
-
-import cv2
+from metrics import *
 
 if __name__ == "__main__":
 
     # path of the image
-    file_image = "images/lssd9.jpg"
+    file_image = "images/test1.jpg"
+    image = cv2.imread(file_image, cv2.IMREAD_COLOR)
 
     cv2.imshow("Original Image", cv2.imread(file_image))
 
@@ -14,9 +17,9 @@ if __name__ == "__main__":
 
     e1 = cv2.getTickCount()
 
-    #shadow_mask = cv2.imread("shadow_masks/93-1.png",cv2.IMREAD_GRAYSCALE)
-    #shadow_mask = first_detection(file_image,False)
-    shadow_mask = second_detection(file_image, False)
+    # shadow_mask = cv2.imread("shadow_masks/test1.png",cv2.IMREAD_GRAYSCALE)
+    shadow_mask = first_detection(file_image, False)
+    # shadow_mask = second_detection(file_image, False)
 
     e2 = cv2.getTickCount()
     time1 = round((e2 - e1) / cv2.getTickFrequency(), 4)
@@ -27,8 +30,8 @@ if __name__ == "__main__":
 
     e3 = cv2.getTickCount()
 
-    first_removal(file_image, shadow_mask, None, False)
-    #second_removal(file_image, shadow_mask, "inpainting")
+    # first_removal(file_image, shadow_mask, None, False)
+    # second_removal(file_image, shadow_mask, "inpainting")
 
     e4 = cv2.getTickCount()
     time2 = round((e4 - e3) / cv2.getTickFrequency(), 4)
@@ -38,6 +41,51 @@ if __name__ == "__main__":
 
     # the results are saved in the results folder named respectively
 
+    image_file_name = str.split((str.split(file_image, "/")[-1]), ".")[0]
+
+    # mse calculation for shadow binary masks
+    if os.path.exists("shadow_masks/" + image_file_name + ".png") or os.path.exists(
+            "shadow_masks/" + image_file_name + ".jpg"):
+
+        if os.path.exists("shadow_masks/" + image_file_name + ".png"):
+            gt_mask_file = "shadow_masks/" + image_file_name + ".png"
+        else:
+            gt_mask_file = "shadow_masks/" + image_file_name + ".jpg"
+
+        gt_mask = cv2.imread(gt_mask_file, cv2.IMREAD_GRAYSCALE)
+
+        mse = mean_square_error(shadow_mask, gt_mask)
+
+        print("The mean square error of the shadow detection is: " + str(mse))
+
+    # mse calculation for images
+    if os.path.exists("ground_truth_images/" + image_file_name + ".png") or os.path.exists("ground_truth_images/" + image_file_name + ".jpg"):
+
+        if os.path.exists("ground_truth_images/" + image_file_name + ".png"):
+            gt_image_file = "ground_truth_images/" + image_file_name + ".png"
+        else:
+            gt_image_file = "ground_truth_images/" + image_file_name + ".jpg"
+
+        gt_image = cv2.imread(gt_image_file, cv2.IMREAD_COLOR)
+
+        mse = mean_square_error(image, gt_image)
+
+        print("The mean square error of the image is: " + str(mse))
+
+    # psnr calculation
+    if os.path.exists("ground_truth_images/" + image_file_name + ".png") or os.path.exists("ground_truth_images/" + image_file_name + ".jpg"):
+
+        if os.path.exists("ground_truth/" + image_file_name + ".png"):
+            gt_image_file = "ground_truth/" + image_file_name + ".png"
+        else:
+            gt_image_file = "ground_truth/" + image_file_name + ".jpg"
+
+        gt_image = cv2.imread(gt_image_file, cv2.IMREAD_COLOR)
+        image = cv2.imread(image_file_name, cv2.IMREAD_COLOR)
+
+        psnr = cv2.PSNR(image, gt_image, 255)
+
+        print("The peak signal to noise ratio is: " + psnr)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
---
+++ metrics.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+import math
+
+# calculates the mean square error
+def mean_square_error(image, gt_image):
+
+    image_shape = image.shape[:2]
+    gt_image_shape = gt_image.shape[:2]
+
+    if image_shape[0] != gt_image_shape[0] or image_shape[1] != gt_image_shape[1]:
+        print("The two input image size is not equal")
+        return
+
+    if len(image_shape) != len(gt_image_shape):
+        print("Not equal channel numbers")
+        return
+
+    if len(image_shape) == 2:
+        return round((np.mean((gt_image - image) ** 2)), 2)
+
+    else:
+        b, g, r = cv2.split(image)
+        bt, gt, rt = cv2.split(gt_image)
+
+        b_mse = np.mean((bt - b) ** 2)
+        g_mse = np.mean((gt - g) ** 2)
+        r_mse = np.mean((rt - r) ** 2)
+
+        return round(((b_mse + g_mse + r_mse)/3) * 100, 2)
+
+
+
+
